11499f738ba2f0fbff94d529024851e0fb6bddf7
liangsen <liangsen20z@ict.ac.cn>
2025-06-11 15:01:32 +0800

    feat(cmo): add fence for cmo

--- am/src/linknan/src/cmo.c
+++ am/src/linknan/src/cmo.c
@@ -5,6 +5,7 @@ void mem_invalid(const volatile uint8_t *array, uint64_t size) {
   while(i --> 0) {
     riscv_cbo_inval((uint64_t)(&(array[i * CACHELINE_SIZE])));
   }
+  riscv_fence(); // Nanhu-V5 needs this to ensure all cmo are done
 }
 
 void mem_flush(const volatile uint8_t *array, uint64_t size) {
@@ -12,4 +13,5 @@ void mem_flush(const volatile uint8_t *array, uint64_t size) {
   while(i --> 0) {
     riscv_cbo_flush((uint64_t)(&(array[i * CACHELINE_SIZE])));
   }
+  riscv_fence(); // Nanhu-V5 needs this to ensure all cmo are done
 }

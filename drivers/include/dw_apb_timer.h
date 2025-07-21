67618e77335fc74e3d1e2da190c6d1d4275fc83e
Fawang <fwcheung@outlook.com>
2025-07-21 10:43:45 +0800

    feat: add dw apb timer driver

---
+++ drivers/include/dw_apb_timer.h
@@ -0,0 +1,25 @@
+#ifndef __DW_APB_TIMER_H__
+#define __DW_APB_TIMER_H__
+
+#include <am.h>
+#include <platform.h>
+
+#define TIMER_BASE(n)			0x50000000L + (n) * 0x10000
+#define APBTMR_N_LOAD_COUNT		0x00
+#define APBTMR_N_CURRENT_VALUE	0x04
+#define APBTMR_N_CONTROL		0x08
+#define APBTMR_N_EOI			0x0c
+#define APBTMR_N_INT_STATUS		0x10
+
+#define APBTMR_CONTROL_ENABLE		(1 << 0)
+/* 1: periodic, 0:free running. */
+#define APBTMR_CONTROL_MODE_PERIODIC	(1 << 1)
+#define APBTMR_CONTROL_INT		(1 << 2)
+
+inline void timer_eoi(int timer);
+void timer_enable_int(int timer);
+void timer_disable_int(int timer);
+void timer_set_periodic(int timer, uint32_t load_count);
+void timer_irq_handler(int timer);
+
+#endif /* __DW_APB_TIMER_H__ */
---
+++ drivers/src/dw_apb_timer.c
@@ -0,0 +1,36 @@
+#include <dw_apb_timer.h>
+
+inline void timer_eoi(int timer) {
+    READ_U32(TIMER_BASE(timer) + APBTMR_N_EOI);
+}
+
+void timer_enable_int(int timer) {
+    uint32_t ctrl = READ_U32(TIMER_BASE(timer) + APBTMR_N_CONTROL);
+    /* clear pending intr */
+    timer_eoi(timer);
+    ctrl &= ~APBTMR_CONTROL_INT;
+    WRITE_U32(TIMER_BASE(timer) + APBTMR_N_CONTROL, ctrl);
+}
+
+void timer_disable_int(int timer) {
+    uint32_t ctrl = READ_U32(TIMER_BASE(timer) + APBTMR_N_CONTROL);
+    ctrl |= APBTMR_CONTROL_INT;
+    WRITE_U32(TIMER_BASE(timer) + APBTMR_N_CONTROL, ctrl);
+}
+
+void timer_set_periodic(int timer, uint32_t load_count) {
+    uint32_t ctrl = READ_U32(TIMER_BASE(timer) + APBTMR_N_CONTROL);
+    ctrl |= APBTMR_CONTROL_MODE_PERIODIC;
+    WRITE_U32(TIMER_BASE(timer) + APBTMR_N_CONTROL, ctrl);
+
+    ctrl &= ~APBTMR_CONTROL_ENABLE;
+    WRITE_U32(TIMER_BASE(timer) + APBTMR_N_CONTROL, ctrl);
+
+    WRITE_U32(TIMER_BASE(timer) + APBTMR_N_LOAD_COUNT, load_count);
+    ctrl |= APBTMR_CONTROL_ENABLE;
+    WRITE_U32(TIMER_BASE(timer) + APBTMR_N_CONTROL, ctrl);
+}
+
+void timer_irq_handler(int timer) {
+    timer_eoi(timer);
+}

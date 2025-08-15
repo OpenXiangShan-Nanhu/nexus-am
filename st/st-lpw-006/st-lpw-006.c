495c09942644828e2aa48e08720c778542e1f1ed
Fawang <fwcheung@outlook.com>
2025-08-15 14:29:54 +0800

    st(lpw): add case st-lpw-006

---
+++ st/st-lpw-006/st-lpw-006.c
@@ -0,0 +1,46 @@
+#include <am.h>
+#include <klib.h>
+#include <klib-macros.h>
+#include <printf.h>
+#include "cmo.h"
+#include "ppu.h"
+#include "clint.h"
+#include "mtrap.h"
+#include "csr.h"
+#include "platform.h"
+
+#define NUM_CORES 4
+
+volatile int step = 0;
+
+void task0() {
+
+  for(volatile int i = 1; i < NUM_CORES; i++) switch_on_core(i);
+
+  while(step != 1);
+  for(volatile int i = 0; i < 100; i++);
+  
+  switch_off_core(1);
+  switch_on_core(1);
+}
+
+void task1(uint64_t id) {
+
+  if(step != 0) { // run at second power-on
+    return;
+  }
+
+  step++; // 1
+  riscv_cbo_flush((uint64_t)&step);
+  riscv_wfi();
+}
+
+void empty(){}
+void (*cpu[NUM_CORES])() = {task0, task1, empty, empty};
+
+int main() {
+  uint64_t hartid = riscv_mhartid();
+  cpu[hartid]();
+  barrier(NUM_CORES);
+  return 0;
+}

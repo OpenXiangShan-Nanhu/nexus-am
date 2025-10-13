301c34d7b4f00493bf7d8e7e4a9d7e448d519cbe
Fawang <fwcheung@outlook.com>
2025-10-13 10:28:57 +0800

    st(int): add case st-int-012

---
+++ st/st-int-012/st-int-012.c
@@ -0,0 +1,59 @@
+#include <am.h>
+#include <klib.h>
+#include <klib-macros.h>
+#include <printf.h>
+#include "cmo.h"
+#include "ppu.h"
+#include "clint.h"
+#include "mtrap.h"
+#include "csr.h"
+#include "platform.h"
+
+#define NUM_CORES 4
+
+volatile int step = 0;
+
+extern void atomic_add(uint64_t *addr, uint64_t val);
+
+void ipi_handler() {
+  uint64_t id = riscv_mhartid();
+  atomic_printf("Core %d get ipi!\n", id);
+  clear_ipi(id);
+  if(id == NUM_CORES-1) return;
+  atomic_printf("Core %d try ipi to core %d\n", id, id+1);
+  raise_ipi(id+1);
+}
+
+int ipi_init() {
+  csr_set(mie, MSIE);
+  csr_set(mstatus, (0x1UL << 3));
+  return 0;
+}
+
+void task0() {
+  for(volatile int i = 1; i < NUM_CORES; i++) switch_on_core(i);
+  while(step != 3);
+
+  for(volatile int i = 0; i < 100; i++);
+  atomic_printf("Core 0 try ipi to core 1\n");
+  raise_ipi(1);
+}
+
+void task1() {
+  ipi_init();
+  if(m_trap_handler_register(MSIP, ipi_handler)) return;
+
+  atomic_add((uint64_t *)&step, 1);
+  riscv_wfi();
+}
+
+void empty(){}
+void (*cpu[NUM_CORES])() = {task0, task1, task1, task1};
+
+int main() {
+
+  uint64_t hartid = riscv_mhartid();
+  cpu[hartid]();
+  barrier(NUM_CORES);
+  return 0;
+}
